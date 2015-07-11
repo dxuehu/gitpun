12baab4762d3f95312ca91e07f7e01ce7c2360c2
Xue Hu <dxuehu@gmail.com>
2015-07-10 20:42:01 -0700

    (Feat) Try separating bundled libraries and our own js

--- client/js/app.js
+++ client/js/app.js
@@ -1,39 +1,6 @@
-var React = require('react');
-var Router = require('react-router');
-var Route = Router.Route;
-var RouteHandler = Router.RouteHandler;
-
-var Landing = React.createClass({
-  render: function () {
-    return <h2>Landing</h2>;
-  }
-});
-
-var AllFiles = React.createClass({
-  render: function () {
-    return <h2>All Files</h2>;
-  }
-});
-
-// declare our routes and their hierarchy
-var routes = (
-  <Route handler={App}>
-    <Route path="landing" handler={Landing}/>
-    <Route path="all-files" handler={AllFiles}/>
-  </Route>
-);
-
-var App = React.createClass({
-  render () {
-    return (
-      <div>
-        <h1>App</h1>
-        <RouteHandler/>
-      </div>
-    )
-  }
-});
-
-Router.run(routes, Router.HashLocation, function (Root) {
-  React.render(<Root/>, document.body);
-});
+var _ = require('underscore');
+//require('js;');require jsx
+//other dependencies here
+//
+//var React = require('react');
+//var Router = require('react-router');
